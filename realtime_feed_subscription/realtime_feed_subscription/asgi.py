1bd02397e1aee38a2ca6f9809222908a3c322f54
akhtar21yr <akhtar.decy@gmail.com>
2024-03-22 18:07:01 +0530

    setup asgi.py to handle websocket and http request

--- realtime_feed_subscription/realtime_feed_subscription/asgi.py
+++ realtime_feed_subscription/realtime_feed_subscription/asgi.py
@@ -10,7 +10,17 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from feeds.routings import websocket_urlpatterns
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realtime_feed_subscription.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime_feed_subscription.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns) 
+        )
+    }
+)
