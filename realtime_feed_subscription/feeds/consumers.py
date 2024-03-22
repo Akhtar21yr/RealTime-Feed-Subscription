94596d0daf9efafbc63cde68d717d08b07bda1b2
akhtar21yr <akhtar.decy@gmail.com>
2024-03-22 19:53:57 +0530

    adding user into group and creating handelrer for sending data in group

--- realtime_feed_subscription/feeds/consumers.py
+++ realtime_feed_subscription/feeds/consumers.py
@@ -5,7 +5,15 @@ class BinanceConsumer(AsyncJsonWebsocketConsumer):
 
     async def connect(self):
         await self.accept()
-        await self.send_json({'msg':'websocket connected'})
+        self.group_name = 'binance'
+        await self.channel_layer.group_add(self.group_name,self.channel_name)
+        await self.channel_layer.group_send(self.group_name,{
+            'type' : 'send.data'
+        }
+        )
+
+    async def send_data(self,event):
+        await self.send_json({'msg':'msg from group'})
 
     async def receive_json(self, content, **kwargs):
         msg = content.get('message')
