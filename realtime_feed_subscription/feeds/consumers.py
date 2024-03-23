e595a8f1b8f4bdacb9239226a15b2439012489b8
akhtar21yr <akhtar.decy@gmail.com>
2024-03-23 09:24:18 +0530

    update consumer to ensure that only authenticated user can access consumer

--- realtime_feed_subscription/feeds/consumers.py
+++ realtime_feed_subscription/feeds/consumers.py
@@ -7,23 +7,29 @@ class BinanceConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         await self.accept()
         self.group_name = 'binance'
-        await self.channel_layer.group_add(self.group_name,self.channel_name)
-        await self.channel_layer.group_send(self.group_name,{
-            'type' : 'send.data'
-        }
-        )
+        user = self.scope['user']
+        if user.is_authenticated :
+            await self.channel_layer.group_add(self.group_name,self.channel_name)
+            await self.channel_layer.group_send(self.group_name,{
+                'type' : 'send.data'
+            }
+            )
+        else :
+            await self.send_json({'msg' : "Login credential not provided"})
+            await self.close()
 
-    async def send_data(self,event):
-        async with websockets.connect(
-            "wss://dstream.binance.com/stream?streams=btcusd_perp@bookTicker"
-        ) as ws:
-            async for data in ws:
-                await self.send_json({"data": data})
+        async def send_data(self,event):
+            async with websockets.connect(
+                "wss://dstream.binance.com/stream?streams=btcusd_perp@bookTicker"
+            ) as ws:
+                async for data in ws:
+                    await self.send_json({"data": data})
+    
 
-    async def receive_json(self, content, **kwargs):
-        msg = content.get('message')
-        if msg :
-            self.send_json({'receive_msg': msg})
+        async def receive_json(self, content, **kwargs):
+            msg = content.get('message')
+            if msg :
+                self.send_json({'receive_msg': msg})
 
-    async def disconnect(self, code):
-        StopConsumer()
+        async def disconnect(self, code):
+            StopConsumer()
