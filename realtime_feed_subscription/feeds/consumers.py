704a0f2597eb72570abb6768b4bcfb657e4af594
akhtar21yr <akhtar.decy@gmail.com>
2024-03-23 09:37:48 +0530

    handling exception and error

--- realtime_feed_subscription/feeds/consumers.py
+++ realtime_feed_subscription/feeds/consumers.py
@@ -8,28 +8,34 @@ class BinanceConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
         self.group_name = 'binance'
         user = self.scope['user']
-        if user.is_authenticated :
-            await self.channel_layer.group_add(self.group_name,self.channel_name)
-            await self.channel_layer.group_send(self.group_name,{
-                'type' : 'send.data'
-            }
-            )
+        if not user.is_authenticated :
+            try :
+                await self.channel_layer.group_add(self.group_name,self.channel_name)
+                await self.channel_layer.group_send(self.group_name,{
+                    'type' : 'send.data'
+                }
+                )
+            except Exception as e :
+                await self.send_json({'error':str(e)})
         else :
             await self.send_json({'msg' : "Login credential not provided"})
             await self.close()
 
-        async def send_data(self,event):
+    async def send_data(self,event):
+        try :
             async with websockets.connect(
                 "wss://dstream.binance.com/stream?streams=btcusd_perp@bookTicker"
             ) as ws:
                 async for data in ws:
                     await self.send_json({"data": data})
+        except Exception as e :
+            await self.send_json({'error':str(e)})
     
 
-        async def receive_json(self, content, **kwargs):
-            msg = content.get('message')
-            if msg :
-                self.send_json({'receive_msg': msg})
+    async def receive_json(self, content, **kwargs):
+        msg = content.get('message')
+        if msg :
+            self.send_json({'receive_msg': msg})
 
-        async def disconnect(self, code):
-            StopConsumer()
+    async def disconnect(self, code):
+        StopConsumer()
