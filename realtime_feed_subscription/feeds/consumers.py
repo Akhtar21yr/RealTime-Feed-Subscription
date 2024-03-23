6f6f3612f9a5b93ea2460a59c3f587cf1dcbe99b
akhtar21yr <akhtar.decy@gmail.com>
2024-03-23 12:36:00 +0530

    handle error

--- realtime_feed_subscription/feeds/consumers.py
+++ realtime_feed_subscription/feeds/consumers.py
@@ -24,7 +24,7 @@ class BinanceConsumer(AsyncJsonWebsocketConsumer):
                 await self.send_json({'error':str(e)})
                 await self.close()
         else :
-            await self.send_json({'msg' : "Login credential not provided"})
+            await self.send_json({'error' : self.scope['error']})
             await self.close()
 
     async def send_data(self,event):
